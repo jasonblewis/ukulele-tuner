49793d21c8d2e16d8541993f4423360590be7fef
Jason Lewis <jason@dickson.st>
2017-04-19 22:18:41 +1000

    remove old code

--- js/scripts.js
+++ js/scripts.js
@@ -33,35 +33,6 @@ var strings = ["g","c","e","a"];
 
 var currentlyPlaying;
 
-function myLoop(element,note) {
-    if (typeof(currentlyPlaying) != 'undefined') {
-	standard.stop(currentlyPlaying);
-    }
-    
-    standard.stop();
-    if (note != currentlyPlaying) {
-	standard.play(note);
-	currentlyPlaying = note;
-	var el = document.getElementsByClassName("active");
-	for (var i = 0; i < el.length; i++) {
-	    el[i].classList.remove('active');
-	    el[i].classList.remove('btn-primary');
-	    el[i].classList.add('btn-default');
-	    
-	}
-	element.classList.add("active");
-	element.classList.add('btn-primary');
-	element.classList.remove('btn-default');
-	
-    } else {
-	currentlyPlaying = undefined;
-	element.classList.remove("active");
-	element.classList.remove('btn-primary');
-	element.classList.add('btn-default');
-    }
-    
-}
-
 function buttonPressed() {
     standard.stop();
     if (this.id == currentlyPlaying) {
